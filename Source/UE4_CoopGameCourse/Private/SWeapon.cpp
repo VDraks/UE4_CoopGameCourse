a56e89f00904e0981d4594f3f40549238aa7746e
VDraks <kytinvasya@gmail.com>
2021-03-01 11:12:35 +0300

    add custom collision channel
    71. Add Custom Collision Channel

--- Source/UE4_CoopGameCourse/Private/SWeapon.cpp
+++ Source/UE4_CoopGameCourse/Private/SWeapon.cpp
@@ -76,7 +76,7 @@ void ASWeapon::Fire()
 		FVector TracerEndPoint = TraceEnd;
 		
 		FHitResult Hit;
-		if (GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, ECC_Visibility, QueryParams))
+		if (GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, COLLISION_WEAPON, QueryParams))
 		{
 			AActor* HitActor = Hit.GetActor();
 
