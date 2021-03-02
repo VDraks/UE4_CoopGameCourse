9671737c5b51f3bf190bdf1f035f4f611a113f29
VDraks <kytinvasya@gmail.com>
2021-03-02 09:50:00 +0300

    add bot pulsing
    96. Pulse Material on TakeDamage

--- Source/UE4_CoopGameCourse/Private/AI/STrackerBot.cpp
+++ Source/UE4_CoopGameCourse/Private/AI/STrackerBot.cpp
@@ -48,6 +48,16 @@ void ASTrackerBot::HandleTakeDamage(USHealthComponent* HealthComponent, float He
 
 	// TODO Pulse the mat on hit
 
+	if (MatInst == nullptr)
+	{
+		MatInst = MeshComp->CreateAndSetMaterialInstanceDynamicFromMaterial(0, MeshComp->GetMaterial(0));	
+	}
+
+	if (MatInst)
+	{
+		MatInst->SetScalarParameterValue("LastTimeDamageTaken", GetWorld()->TimeSeconds);	
+	}
+
 	UE_LOG(LogTemp, Log, TEXT("Health %s of %s"), *FString::SanitizeFloat(Health), *GetName())
 }
 
