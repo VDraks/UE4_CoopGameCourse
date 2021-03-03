6b43354135256239ab3ece6e6787f2683f5e0ce9
VDraks <kytinvasya@gmail.com>
2021-03-03 10:11:10 +0300

    add wave based spawning
    121. Add Wave Based Spawning

--- Source/UE4_CoopGameCourse/Public/SGameMode.h
+++ Source/UE4_CoopGameCourse/Public/SGameMode.h
@@ -13,5 +13,32 @@ UCLASS()
 class UE4_COOPGAMECOURSE_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	ASGameMode();
+
+protected:
+	FTimerHandle TimerHandle_BotSpawner;
+
+	int32 NrOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	void StartWave();
+
+	void EndWave();
+
+	void PrepareForNextWave();
+
+public:
+	virtual void StartPlay() override;
 };
